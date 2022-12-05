09070e495188711ee766190feabd3c3f88fc4473
Frank Mayer <mail@frank-mayer.io>
2022-12-06 00:56:34 +0100

    Added ESLint

--- source/view/index.tsx
+++ source/view/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot, type Root } from "react-dom/client";
 import { AppView } from "./AppView";
 
 const appEl = document.getElementById("app");
 
 if (appEl) {
-  (globalThis as any).reactRoot = createRoot(appEl);
+  (globalThis as unknown as { reactRoot: Root }).reactRoot = createRoot(appEl);
 
-  (globalThis as any).reactRoot.render(
+  (globalThis as unknown as { reactRoot: Root }).reactRoot.render(
     <React.StrictMode>
       <AppView />
     </React.StrictMode>
