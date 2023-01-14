51faf3d212cb1caa82dd9e3249a6649379001cb2
Frank-Mayer <mail@frank-mayer.io>
2023-01-14 18:40:54 +0100

    message when update is available

--- source/index.tsx
+++ source/index.tsx
@@ -1,9 +1,29 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 import { AppView } from "./view/AppView"
+import { onServiceWorkerCacheUpdated } from "@frank-mayer/service-worker/client"
+import { inject } from "./lib/globalDI"
+import { AppState } from "./types/AppState"
+
+onServiceWorkerCacheUpdated(() => {
+    const appModel = inject("AppModel")
+    if (appModel.state === AppState.MAIN_MENU) {
+        location.reload()
+        return
+    }
+
+    // eslint-disable-next-line no-alert
+    const doReload = confirm([
+        "Für diese App ist ein Update verfügbar.",
+        "Soll die App jetzt neu gestartet werden, um diese Änderung zu laden?"
+    ].join("\n"))
+
+    if (doReload) {
+        location.reload()
+    }
+})
 
 // initialize the react root
-
 const appEl = document.getElementById("app")
 
 if (appEl) {
