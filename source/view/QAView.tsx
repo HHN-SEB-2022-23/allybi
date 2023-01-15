8d2b266baa4d052fed2f4f3f033b0508cd55adde
Frank Mayer <mail@frank-mayer.io>
2023-01-15 12:16:33 +0100

    Q&A Page (#28)
    Co-authored-by: Antonia <antonia-friese@gmx.de>

--- source/view/QAView.tsx
+++ source/view/QAView.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { observer } from "mobx-react"
+import { inject } from "../lib/globalDI"
 
 /**
  * The main menu view.
@@ -7,9 +8,44 @@ import { observer } from "mobx-react"
  * This view is responsible for rendering the main menu.
  */
 export const QAView = observer(() => {
+    const _qaViewModel = inject("QAViewModel")
+
     return (
-        <div className="menu">
+        <div className="menu document">
             <h1 className="menu__title">Q&A</h1>
+            <button onClick={_qaViewModel.onClickBack} className="menu__item">Zurück</button>
+            <h2 className="document__subtitle">Was ist die adi?</h2>
+            <p>
+                Die adi.hn, die Antidiskriminierungsstelle Heilbronn, gibt es
+                seit 2021. Sie ist Anlaufstelle für alle, die Diskriminierung
+                erleben, mehr über Antidiskriminierung erfahren möchten und sich
+                für eine respektvolle Gesellschaft auf Augenhöhe engagieren
+                wollen. Unsere Beratungsangebote und viele der Workshops sind
+                kostenfrei. Wir freuen uns über Kooperationen und Ideen, um
+                gemeinsam im Stadt- und Landkreis diskriminierungssensibel
+                miteinander zu leben.
+                <br />
+
+                <a href="https://www.skjr-hn.de/antidiskriminierungsstelle-heilbronn/" target="_blank" rel="noopener noreferrer"> Seite der adi.hn</a>
+            </p>
+
+            <h2 className="document__subtitle">Wie sieht das Ehrenamt bei der adi aus?</h2>
+            <p>
+                Lass dich als Multiplikator*in für Workshops ausbilden, hilf
+                uns, unseren Social Media Kanal zu betreuen oder Grafiken zu
+                erstellen, begleite uns auf Veranstaltungen oder entwickle
+                eigene Ideen, die wir unterstützen können.Ehrenamt bei der
+                adi.hn ist vielfältig und sinnstiftend.
+            </p>
+
+            <h2 className="document__subtitle">Ich habe kaum Zeit. Kann ich trotzdem helfen?</h2>
+            <p>Folgt noch</p>
+
+            <h2 className="document__subtitle">Wo findet das Ehrenamt statt?</h2>
+            <p>Folgt noch</p>
+
+            <p>Wir freuen uns auf deine Anfrage unter adi@skjr-hn.de</p>
+
         </div>
     )
 })
---
+++ source/viewmodel/QAViewModel.ts
@@ -0,0 +1,20 @@
+import { makeAutoObservable } from "mobx"
+import { inject } from "../lib/globalDI"
+import { MouseEventHandler } from "react"
+
+/**
+ * View model for the options menu
+ *
+ * Sets the options in the options model
+ */
+export class QAViewModel {
+    private readonly _appModel = inject("AppModel")
+
+    public constructor() {
+        makeAutoObservable(this)
+    }
+
+    public readonly onClickBack: MouseEventHandler<Element> = () => {
+        this._appModel.navigateBack()
+    }
+}
