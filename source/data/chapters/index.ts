83d2d30f080353a48a19e14502905479d1faff55
René Ott <rene.ott1997@yahoo.de>
2023-01-19 16:39:50 +0100

    Chapter ausgeklammert für Präsentation

--- source/data/chapters/index.ts
+++ source/data/chapters/index.ts
@@ -1,19 +1,15 @@
 import type { Chapter } from "../../types/Chapter"
 import { alter } from "./alter"
-import { aussehen } from "./aussehen"
 import { geschlecht } from "./geschlecht"
-import { herkunft } from "./herkunft"
-import { religion } from "./religion"
-import { sexualitaet } from "./sexualitaet"
 import { sozialerStatus } from "./sozialerStatus"
 
 export const chapters: Array<Chapter> = [
     alter,
-    aussehen,
+    //aussehen,
     // behinderung,
     geschlecht,
-    herkunft,
-    religion,
-    sexualitaet,
+    //herkunft,
+    //religion,
+    //sexualitaet,
     sozialerStatus
 ]
